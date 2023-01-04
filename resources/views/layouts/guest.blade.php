d37c3909b06cc509b4703de7d1e4dcded27068e1
nmarah <marahnasser6@gmail.com>
2023-01-04 11:42:54 +0200

    add articles table1

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,9 +11,9 @@
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="{{ asset('build/assets/app-cba5cfab.css') }}">
+        <link rel="stylesheet" href="{{ asset('/build/assets/app-cba5cfab.css') }}">
 
-        <script src="{{ asset('build/assets/app-113fdd3a.js') }}"></script>
+        <script src="{{ asset('/build/assets/app-113fdd3a.js') }}"></script>
 
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     </head>
